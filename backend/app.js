152a2593e054746e4dbe55a6cdc6c8135f301cd1
Mukulsh26 <mukulsh7496@gmail.com>
2025-01-27 01:57:20 +0530

    feat: fixed

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/tasks', taskRoutes);
+app.use('/api/tasks', taskRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
